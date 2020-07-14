8a9f07a5b9e9eb4cbbacc1f0d0d4dcc64ee7999d
em06 <em06@elt.local>
2020-07-14 11:43:06 +0900

    Add Temperature sensor

--- UltraSonicDistance.c
+++ UltraSonicDistance.c
@@ -20,15 +20,27 @@ void main() {
 		if (SCI0_RX_FLAG == 1) {
 			if (SCI0_RX_DATA == 'd') {
 				// show distance by SCI
-				unsigned char distance_message[7];
-				distance_message[0] = distance / 1000 % 10 + '0';
-				distance_message[1] = distance / 100 % 10 + '0';
-				distance_message[2] = distance / 10 % 10 + '0';
-				distance_message[3] = distance / 1 % 10 + '0';
-				distance_message[4] = 'c';
-				distance_message[5] = 'm';
-				distance_message[6] = '\n\r';
-				pPrint_SCI( distance_message, 7);
+				unsigned char message[8];
+				message[0] = distance / 1000 % 10 + '0';
+				message[1] = distance / 100 % 10 + '0';
+				message[2] = distance / 10 % 10 + '0';
+				message[3] = distance / 1 % 10 + '0';
+				message[4] = 'c';
+				message[5] = 'm';
+				message[6] = '\r';
+				message[7] = '\n';
+				pPrint_SCI(message, 8);
+			} else if (SCI0_RX_DATA == 't') {
+				unsigned char message[7];
+				unsigned short temperature = getTemperature();
+				message[0] = temperature / 100 % 10 + '0';
+				message[1] = temperature / 10 % 10 + '0';
+				message[2] = '.';
+				message[3] = temperature / 1 % 10 + '0';
+				message[4] = 'c';
+				message[5] = '\r';
+				message[6] = '\n';
+				pPrint_SCI(message, 7);
 			}
 			// reset
 			SCI0_RX_FLAG = 0;
@@ -45,6 +57,7 @@ void setup() {
 	init_TPU9();
 	initIRQ();
 	init_CMT0();
+	init_S12AD();
 	setpsw_i();
 }
 
---
+++ temperature.c
@@ -0,0 +1,54 @@
+#include "iodefine.h"
+#include <machine.h>
+
+unsigned short getTemperature() {
+    unsigned short int data_AN002;
+    unsigned short int cal_AN002;
+    S12AD.ADCSR.BIT.ADST = 1;
+    data_AN002 = S12AD.ADDR2;
+    cal_AN002 = (unsigned short) ((( data_AN002 * 3300 / 4096.0 -424) / 6.25 ) * 10 );
+    return cal_AN002;
+}
+
+void init_S12AD(void) {
+    /* Enable Module S21AD Converter */
+    SYSTEM.PRCR.WORD = 0xA502;
+    SYSTEM.MSTPCRA.BIT.MSTPA17 = 0;
+    SYSTEM.PRCR.WORD = 0xA500;
+    /* P40&42 set Input pin */
+    PORT4.PDR.BIT.B0 = 0;
+    PORT4.PDR.BIT.B2 = 0;
+    
+    /* Setting PMR General I/O */
+    PORT4.PMR.BIT.B0 = 0;
+    PORT4.PMR.BIT.B2 = 0;
+    
+    /***** ADC Setting ******/
+
+    /* Contorol Setting */
+    S12AD.ADCSR.BIT.ADST = 0;  // Stop ADC
+    S12AD.ADCSR.BYTE = 0x1C; 
+    // Stop ADC, SingleScan, reserved, Enable Interrupt, 
+    // Clock PCLK/1, trigger off, sync trigger
+    
+    /* channel select */
+    S12AD.ADANS0.WORD = 0x05;  // ch0 convert AN000 & 002
+	
+    /* AD Data Format setting */
+    S12AD.ADCER.BIT.ADRFMT = 0;    // Right-justfy 
+        
+    /**** MPCでP40端子をアナログ入力端子に設定 ***/
+    MPC.PWPR.BYTE &= ~0x80;    // プロテクト解除
+    MPC.PWPR.BYTE |= 0x40;
+    
+    MPC.P40PFS.BYTE = 0x80;    // アナログ端子に設定(ASELビットを１)
+    MPC.P42PFS.BYTE = 0x80;
+    
+    MPC.PWPR.BYTE &= ~0x40;    // プロテクト設定
+    MPC.PWPR.BYTE |= 0x80;
+    
+    /***** 割り込みの設定 *****/
+    //ICU.IPR[102].BIT.IPR = 8;    // Priority level 8
+    //ICU.IR[102].BIT.IR = 0;     // clear Status Flag
+    //ICU.IER[0x0C].BIT.IEN6 = 1;   // Enable Interrupt
+}
